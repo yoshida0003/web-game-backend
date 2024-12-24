76a20622b79eb6f0aef8edf38446f2a16c4d9dc0
yoshida0003 <ren050800@icloud.com>
2024-12-24 12:28:35 +0900

    second commit

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/api', (req, res) => {
   res.send('hello World');
 });
 
+app.get('/api/test', (req, res) => {
+  res.send('test');
+});
+
 app.listen(3001, () => {
   console.log('Server listening on port 3001');
 });
