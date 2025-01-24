fc8efbd3b521c02aed71595de75e88d2288183e2
yoshida0003 <ren050800@icloud.com>
2025-01-25 02:33:12 +0900

    クライアントのURLの修正

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000", // クライアントのURL
+		origin: "https://game.yospace.org", // クライアントのURL
 		methods: ["GET", "POST"],
 		credentials: true,
 	},
